89effbeae9ca19b689e5bb50902e71707259b5ab
Usman-FE <ch.usman564@gmail.com>
2022-05-06 16:25:35 +0500

    Done testimonials

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,6 @@
-console.log("hello gulp");
+const lightbox = GLightbox({
+  href: "https://www.youtube.com/watch?v=Ga6RYejo6Hk",
+  type: "video",
+  source: "youtube", //vimeo, youtube or local
+  width: 900,
+});
--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,2 @@
-console.log("hello gulp");
+const lightbox=GLightbox({href:"https://www.youtube.com/watch?v=Ga6RYejo6Hk",type:"video",source:"youtube",width:900});
 //# sourceMappingURL=app.js.map
